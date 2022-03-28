a4fdf2709286e44058e29ae93046d2e7b61c937b
Kasia Laniecka <kl@parkshare.dk>
2022-03-28 20:34:16 +0200

    start scene and level 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    // public AudioClip crunchAudioClip;
+    public GameObject finishMessage;
     private AudioSource myAudio;
+    public GameObject[] allCookies;
+    public int cookiesCollected = 0;
+    public TextMeshProUGUI cookiesDisplay;
 
     private Rigidbody2D myRigidbody;
 
-    [Header("-- Private references --")]
-    [SerializeField] private int cookiesCollected = 0;
-    [SerializeField] private GameObject winningFlag;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        allCookies = GameObject.FindGameObjectsWithTag("Cookie");
         myRigidbody = GetComponent<Rigidbody2D>();
         myAudio = GetComponent<AudioSource>();
     }
@@ -27,49 +30,65 @@ public class Player : MonoBehaviour
         
         Vector3 newPosition = transform.position;
         // Quaternion newRotation = transform.rotation;
-            if(Input.GetKey(KeyCode.A)) {
-                // newPosition.x -= moveSpeed * Time.deltaTime;
-                // Quaternion rotation = Quaternion.Euler(0,0,180);
-                myRigidbody.velocity = new Vector2(-5, 0);
-            }
+        if(Input.GetKey(KeyCode.A)) {
+            // newPosition.x -= moveSpeed * Time.deltaTime;
+            // Quaternion rotation = Quaternion.EulerAngles(0,0,180);
+            myRigidbody.velocity = new Vector2(-5, 0);
+        }
 
-            if(Input.GetKey(KeyCode.D)) {
-                // newPosition.x += moveSpeed * Time.deltaTime;
-                myRigidbody.velocity = new Vector2(5, 0);
-            }
+        if(Input.GetKey(KeyCode.D)) {
+            // newPosition.x += moveSpeed * Time.deltaTime;
+            myRigidbody.velocity = new Vector2(5, 0);
+        }
 
-            if(Input.GetKey(KeyCode.W)) {
-                // newPosition.y += moveSpeed * Time.deltaTime;
-                myRigidbody.velocity = new Vector2(0, 5);
-            }
+        if(Input.GetKey(KeyCode.W)) {
+            // newPosition.y += moveSpeed * Time.deltaTime;
+            myRigidbody.velocity = new Vector2(0, 5);
+        }
 
-            if(Input.GetKey(KeyCode.S)) {
-                myRigidbody.velocity = new Vector2(0, -5);
-                // newPosition.y -= moveSpeed * Time.deltaTime;
-            }
+        if(Input.GetKey(KeyCode.S)) {
+            myRigidbody.velocity = new Vector2(0, -5);
+            // newPosition.y -= moveSpeed * Time.deltaTime;
+        }
         transform.position = newPosition;
+
+        
+
+        // if(allCookiesCount.Length == 0) 
+        // {
+        //     print("You won! Cookies: " + allCookiesCount);
+        //     Instantiate(finishMessage);
+        // }
     }
 
-    // OnTriggerEnter2D is called once, when two GameObjects with Collider2Ds hit each other
-        // - One GameObject must have a Rigidbody2D as well
-        // - One of the Collider2Ds must be a Trigger
+
     void OnTriggerEnter2D(Collider2D coll) {
         if(coll.gameObject.tag.Equals("Cookie")) {
             cookiesCollected += 1;
             Destroy(coll.gameObject);
             myAudio.Play(); 
+            // print(cookiesCollected);
+            cookiesDisplay.text = cookiesCollected.ToString();
         }
 
-        // if(coinsCollected == 3) {
-        //     Instantiate(winningFlag);
-        // }
+
+
+        // if(cookiesCollected == 5)
+        //     {
+        //         Instantiate(finishMessage);
+        //     }
+
+        // foreach (GameObject cookie in allCookies)
+        //     {
+        //         if(cookie == null) 
+        //             {
+                        
+        //             }
+        //         print(allCookies);
+        //     // print("You won! Cookies: " + .Length);
+                
+        //     }
+
     }
 
-    // OnCollisionEnter2D is called once, when two GameObjects with Collider2Ds hit each other
-        // - One GameObject must have a Rigidbody2D as well
-    void OnCollisionEnter2D(Collision2D coll) {
-        // if(coll.gameObject.tag.Equals("Finish")) {
-        //     print("You have won the game!");
-        // }
-    }
 }
